2ab0b5e7fb7043d99bf1a7ba6ffa173df936f05b
Siniša Marković <sinisa.markovic.contact@gmail.com>
2024-02-24 16:04:24 +0100

    feat: add option for catch2

--- cmake/StandardSettings.cmake
+++ cmake/StandardSettings.cmake
@@ -11,6 +11,8 @@ option(${PROJECT_NAME}_ENABLE_UNIT_TESTING "Enable unit tests for the projects (
 option(${PROJECT_NAME}_USE_GTEST "Use the GoogleTest project for creating unit tests." ON)
 option(${PROJECT_NAME}_USE_GOOGLE_MOCK "Use the GoogleMock project for extending the unit tests." OFF)
 
+option(${PROJECT_NAME}_USE_CATCH2 "Use the Catch2 project for creating unit tests." OFF)
+
 # Static analyzers
 # Currently supporting: Clang-Tidy, Cppcheck.
 option(${PROJECT_NAME}_ENABLE_CLANG_TIDY "Enable static analysis with Clang-Tidy." OFF)
